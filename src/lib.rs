6b173b16f6d9d2dece8217650e399e3127eeacd8
Roman Yurchak <rth.yurchak@pm.me>
2018-11-11 12:22:21 +0100

    Use sparse arrays

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,17 @@ extern crate lazy_static;
 extern crate fasthash;
 extern crate fnv;
 extern crate regex;
+extern crate sprs;
 
 use fnv::FnvHashMap;
 use regex::Regex;
+use sprs::CsMat;
+
 
 const TOKEN_PATTERN_DEFAULT: &str = r"(?-u:\b)\w\w+(?-u:\b)";
 
 #[cfg(test)]
 mod tests;
-
-#[derive(Debug)]
-pub struct CSRArray {
-    indices: Vec<u32>,
-    indptr: Vec<u64>,
-    values: Vec<i32>,
-}
 
 pub fn analyze(tokens: Vec<&str>) -> Vec<&str> {
     tokens
@@ -41,6 +37,14 @@ pub fn count(tokens: Vec<&str>) -> (usize) {
 }
 
 #[derive(Debug)]
+struct _CSRArray {
+    pub indices: Vec<usize>,
+    pub indptr: Vec<usize>,
+    pub values: Vec<i32>,
+}
+
+
+#[derive(Debug)]
 pub struct HashingVectorizer {
     lowercase: bool,
     token_pattern: String,
@@ -49,7 +53,7 @@ pub struct HashingVectorizer {
 
 impl HashingVectorizer {
     pub fn new() -> Self {
-        // Create a new HashingVectorizer class
+        // Create a new HashingVectorizer estimator
         HashingVectorizer {
             lowercase: true,
             token_pattern: String::from(TOKEN_PATTERN_DEFAULT),
@@ -64,10 +68,10 @@ impl HashingVectorizer {
         self
     }
 
-    pub fn transform(&self, X: &[String]) -> CSRArray {
+    pub fn transform(&self, X: &[String]) -> CsMat<i32> {
         // Transform method
 
-        let mut tf = CSRArray {
+        let mut tf = _CSRArray {
             indices: Vec::new(),
             indptr: Vec::new(),
             values: Vec::new(),
@@ -84,15 +88,20 @@ impl HashingVectorizer {
                 let hash = fasthash::murmur3::hash32(&token);
                 let bucket = hash % self.n_features;
 
-                tf.indices.push(bucket);
+                tf.indices.push(bucket as usize);
                 tf.values.push(1);
             }
-            tf.indptr.push(tf.values.len() as u64);
+            tf.indptr.push(tf.values.len());
         }
-        tf
+        CsMat::new(
+            (tf.indptr.len() - 1, self.n_features as usize),
+            tf.indptr,
+            tf.indices,
+            tf.values,
+        )
     }
 
-    pub fn fit_transform(&self, X: &[String]) -> CSRArray {
+    pub fn fit_transform(&self, X: &[String]) -> CsMat<i32> {
         // Fit and transform
         //
         self.transform(X)
--- src/tests.rs
+++ src/tests.rs
@@ -1,13 +1,35 @@
 use *;
 
+extern crate sprs;
+use sprs::CsMat;
+
 #[test]
 fn hashing_vectorizer_init() {
-    let documents = vec![String::from("sky is blue"), String::from("sea is blue")];
+    // >>> vect = HashingVectorizer(norm=None, alternate_sign=False)
+    // >>> X = vect.fit_transform(['the moon in the sky', 'The sky is blue'])
+    // >>> X.indices
+    // array([268391, 286878, 720286, 828689, 144749, 268391, 286878, 790269],
+    //       dtype=int32)
+    // >>> X.indptr
+    // array([0, 4, 8], dtype=int32)
+    // >>> X.data
+    // array([1., 2., 1., 1., 1., 1., 1., 1.])
+    let documents = vec![
+        String::from("the moon in the sky"),
+        String::from("The sky is blue"),
+    ];
 
-    let vect = HashingVectorizer::new();
-    let vect = vect.fit(&documents);
-    let X = vect.transform(&documents);
-    println!("{:?}", X);
+    //let vect = HashingVectorizer::new();
+    //let vect = vect.fit(&documents);
+    //let X = vect.transform(&documents);
+    //println!("{:?}", X);
+    //let indptr: Vec<usize> = Vec::from(X.indptr());
+    //assert_eq!(indptr, vec![0, 4, 8])
+    //
+    let indptr = vec![0, 5, 8];
+    let indices = vec![761698, 328290, 828689, 761698, 780185, 901149, 780185, 144749, 258307];
+    let data = vec![1, 1, 1, 1, 1, 1, 1, 1, 1];
+    let a = CsMat::new_csc((2, 1000000), indptr, indices, data);
 
     //let res2 = vect.fit_transform(&X);
     //assert_eq!(res, res2);
