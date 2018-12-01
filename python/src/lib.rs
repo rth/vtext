871e58e630f417dd0565ebe99a9e05aeda71bcf2
Roman Yurchak <rth.yurchak@pm.me>
2018-12-01 09:35:11 +0100

    Preliminary wrapper for hasher

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -13,10 +13,25 @@ use text_vectorize::CountVectorizer;
 
 use text_vectorize::tokenize;
 
+fn vec_usize_to_i32(vec: Vec<usize>) -> Vec<i32> {
+    let mut vect_out: Vec<i32> = Vec::new();
+    for element in vec.iter() {
+        if *element > std::i32::MAX as usize {
+            panic!("Cannot safely coerce indices to i32!");
+        } else {
+            vect_out.push(*element as i32);
+        }
+    }
+    vect_out
+}
+
 #[pymodinit]
 fn _lib(_py: Python, m: &PyModule) -> PyResult<()> {
-    #[pyfn(m, "count_vectorize")]
-    fn count_vectorize(py: Python, x: PyObject) -> PyResult<(Py<PyArray1<i32>>)> {
+    #[pyfn(m, "hash_vectorize")]
+    fn hash_vectorize(
+        py: Python,
+        x: PyObject,
+    ) -> PyResult<(Py<PyArray1<i32>>, Py<PyArray1<i32>>, Py<PyArray1<i32>>)> {
         let text = PyIterator::from_object(py, &x)?;
 
         let mut collection: Vec<String> = Vec::new();
@@ -33,9 +48,12 @@ fn _lib(_py: Python, m: &PyModule) -> PyResult<()> {
         println!("{:?}", collection);
         println!("{:?}", x);
 
+        let indices = vec_usize_to_i32(x.indices);
+        let indptr = vec_usize_to_i32(x.indptr);
+
         Ok((
-            x.indices.into_pyarray(py).to_owned(),
-            x.indptr.into_pyarray(py).to_owned(),
+            indices.into_pyarray(py).to_owned(),
+            indptr.into_pyarray(py).to_owned(),
             x.data.into_pyarray(py).to_owned(),
         ))
     }
