d300f3fe0955b2f2c04b7ddf27e94609b0855dc8
Roman Yurchak <rth.yurchak@pm.me>
2018-12-01 08:58:05 +0100

    Accepting iterator as input

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,40 +1,43 @@
 #![feature(specialization)]
 
 #[macro_use]
-extern crate pyo3;
 extern crate ndarray;
 extern crate numpy;
+extern crate pyo3;
 extern crate text_vectorize;
 
-use numpy::{IntoPyArray, PyArrayDyn};
-use pyo3::prelude::{pymodinit, Py, PyModule, PyResult, Python};
+use numpy::{IntoPyArray, PyArray1};
+use pyo3::prelude::{pymodinit, ObjectProtocol, Py, PyModule, PyObject, PyResult, Python};
+use pyo3::types::PyIterator;
+use text_vectorize::CountVectorizer;
 
 use text_vectorize::tokenize;
 
 #[pymodinit]
 fn _lib(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "count_vectorize")]
-    fn count_vectorize(
-        text: Py<PyArrayDyn<i32>>,
-    ) -> (
-        Py<PyArrayDyn<i32>>,
-        Py<PyArrayDyn<i32>>,
-        Py<PyArrayDyn<i32>>,
-    ) {
-        let indices: Vec<i32> = Vec::new();
-        let indptr: Vec<i32> = Vec::new();
-        let values: Vec<i32> = Vec::new();
-        indices.push(0);
-        indptr.push(1);
-        values.push(1);
+    fn count_vectorize(py: Python, x: PyObject) -> PyResult<(Py<PyArray1<i32>>)> {
+        let text = PyIterator::from_object(py, &x)?;
 
-        // XXX: add actual call to text_vectorize
+        let mut collection: Vec<String> = Vec::new();
 
-        (
-            indices.into_pyarray(_py).to_owned(),
-            values.into_pyarray(_py).to_owned(),
-            indptr.into_pyarray(_py).to_owned(),
-        )
+        for document in text {
+            let document = document?;
+            let document = ObjectProtocol::extract::<String>(document)?;
+            collection.push(document);
+        }
+
+        let mut vect = CountVectorizer::new();
+        let x = vect.fit_transform(&collection);
+
+        println!("{:?}", collection);
+        println!("{:?}", x);
+
+        Ok((
+            x.indices.into_pyarray(py).to_owned(),
+            x.indptr.into_pyarray(py).to_owned(),
+            x.data.into_pyarray(py).to_owned(),
+        ))
     }
 
     Ok(())
