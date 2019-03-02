c71e9c881a2516ff8a136de3629853331d5b5d80
Roman Yurchak <rth.yurchak@pm.me>
2019-03-02 19:01:40 +0100

    Add RegexpTokenizer implementation

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,8 +1,10 @@
 extern crate unicode_segmentation;
+extern crate regex;
 
+use regex::Regex;
 use unicode_segmentation::UnicodeSegmentation;
 
-/// Tokenize with Unicode Segmentation
+/// Unicode Segmentation tokenizer
 ///
 /// This implementation is a thin wrapper around the
 /// `unicode-segmentation` crate
@@ -27,9 +29,29 @@ impl UnicodeSegmentTokenizer {
     }
 }
 
+const TOKEN_PATTERN_DEFAULT: &str = r"\b\w\w+\b";
+
+/// Regular expression tokenizer
+///
+#[derive(Debug)]
+pub struct RegexpTokenizer {
+    pub pattern: String,
+}
+
+impl RegexpTokenizer {
+    /// Tokenize a string
+    pub fn tokenize<'a>(&self, text: &'a str) -> Vec<&'a str> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(TOKEN_PATTERN_DEFAULT).unwrap();
+        }
+
+        RE.find_iter(text).map(|m| m.as_str()).collect() //.collect::Vec<&str>
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::tokenize::UnicodeSegmentTokenizer;
+    use crate::tokenize::{UnicodeSegmentTokenizer,RegexpTokenizer};
 
     #[test]
     fn test_unicode_tokenizer() {
@@ -48,6 +70,16 @@ mod tests {
             "The", "quick", "(", "\"", "brown", "\"", ")", "fox", "can't", "jump", "32.3", "feet",
             ",", "right", "?",
         ];
+        assert_eq!(tokens, b);
+    }
+
+    #[test]
+    fn test_regexp_tokenizer() {
+        let s = "fox can't jump 32.3 feet, right?";
+
+        let tokenizer = RegexpTokenizer { pattern: r"\b\w\w+\b".to_string() };
+        let tokens = tokenizer.tokenize(s);
+        let b: &[_] = &["fox", "can", "jump", "32", "feet", "right"];
         assert_eq!(tokens, b);
     }
 }
