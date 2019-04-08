4da817bf7c31c2297c2201d53c60138cc8095fb9
Roman Yurchak <rth.yurchak@pm.me>
2019-04-09 00:49:08 +0200

    Add VTextTokenizer

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -4,6 +4,30 @@ extern crate unicode_segmentation;
 use regex::Regex;
 use unicode_segmentation::UnicodeSegmentation;
 
+/// Regular expression tokenizer
+///
+#[derive(Debug)]
+pub struct RegexpTokenizer {
+    pub pattern: String,
+    regexp: Regex,
+}
+
+impl RegexpTokenizer {
+    /// Create a new instance
+    pub fn new(pattern: String) -> RegexpTokenizer {
+        let regexp = Regex::new(&pattern).unwrap();
+
+        RegexpTokenizer {
+            pattern: pattern,
+            regexp: regexp,
+        }
+    }
+    /// Tokenize a string
+    pub fn tokenize<'a>(&'a self, text: &'a str) -> impl Iterator<Item = &'a str> {
+        self.regexp.find_iter(text).map(|m| m.as_str())
+    }
+}
+
 /// Unicode Segmentation tokenizer
 ///
 /// This implementation is a thin wrapper around the
@@ -35,33 +59,62 @@ impl UnicodeSegmentTokenizer {
     }
 }
 
-/// Regular expression tokenizer
+/// vtext tokenizer
 ///
+/// This tokenizer builds upon the `unicode-segmentation` crate
+///
+/// ## References
+///
+/// * [Unicode® Standard Annex #29](http://www.unicode.org/reports/tr29/)
 #[derive(Debug)]
-pub struct RegexpTokenizer {
-    pub pattern: String,
-    regexp: Regex,
+pub struct VTextTokenizer {
+    pub lang: String,
 }
 
-impl RegexpTokenizer {
+impl VTextTokenizer {
     /// Create a new instance
-    pub fn new(pattern: String) -> RegexpTokenizer {
-        let regexp = Regex::new(&pattern).unwrap();
-
-        RegexpTokenizer {
-            pattern: pattern,
-            regexp: regexp,
+    pub fn new(lang: &str) -> VTextTokenizer {
+        VTextTokenizer {
+            lang: lang.to_string()
         }
     }
     /// Tokenize a string
-    pub fn tokenize<'a>(&'a self, text: &'a str) -> impl Iterator<Item = &'a str> {
-        self.regexp.find_iter(text).map(|m| m.as_str())
+    pub fn tokenize<'a>(&self, text: &'a str) -> Box<Iterator<Item = &'a str> + 'a> {
+        let tokens = text.split_word_bounds();
+
+        let mut res: Vec<&'a str> = Vec::new();
+        for tok in tokens {
+            let n_chars = tok.chars().count();
+            if tok.ends_with(&"n't") {
+               res.push(&tok[0..n_chars-3]);
+               res.push(&tok[n_chars-3..n_chars]);
+            } else if tok.ends_with(&"'s") {
+               res.push(&tok[0..n_chars-2]);
+               res.push(&tok[n_chars-2..n_chars]);
+            } else {
+               res.push(tok);
+            }
+        }
+        // remove whitespace tokens
+        let res = res.into_iter().filter(|x| x != &" ");
+        return Box::new(res);
     }
 }
 
+
 #[cfg(test)]
 mod tests {
-    use crate::tokenize::{RegexpTokenizer, UnicodeSegmentTokenizer};
+    use crate::tokenize::{RegexpTokenizer, UnicodeSegmentTokenizer, VTextTokenizer};
+
+    #[test]
+    fn test_regexp_tokenizer() {
+        let s = "fox can't jump 32.3 feet, right?";
+
+        let tokenizer = RegexpTokenizer::new(r"\b\w\w+\b".to_string());
+        let tokens: Vec<&str> = tokenizer.tokenize(s).collect();
+        let b: &[_] = &["fox", "can", "jump", "32", "feet", "right"];
+        assert_eq!(tokens, b);
+    }
 
     #[test]
     fn test_unicode_tokenizer() {
@@ -84,12 +137,19 @@ mod tests {
     }
 
     #[test]
-    fn test_regexp_tokenizer() {
-        let s = "fox can't jump 32.3 feet, right?";
+    fn test_vtext_tokenizer() {
 
-        let tokenizer = RegexpTokenizer::new(r"\b\w\w+\b".to_string());
+        let tokenizer = VTextTokenizer::new("en");
+
+        let s = "We can't";
         let tokens: Vec<&str> = tokenizer.tokenize(s).collect();
-        let b: &[_] = &["fox", "can", "jump", "32", "feet", "right"];
-        assert_eq!(tokens, b);
+        let tokens_ref: &[_] = &["We", "ca", "n't"];
+        assert_eq!(tokens, tokens_ref);
+
+        let s = "it's";
+        let tokens: Vec<&str> = tokenizer.tokenize(s).collect();
+        let tokens_ref: &[_] = &["it", "'s"];
+        assert_eq!(tokens, tokens_ref);
     }
+
 }
