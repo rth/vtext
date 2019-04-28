7b2636774d631824346eeb2b5f9836280465cdf1
Roman Yurchak <rth.yurchak@pm.me>
2019-04-28 20:56:20 +0200

    Add tests

--- src/metrics/string.rs
+++ src/metrics/string.rs
@@ -14,7 +14,7 @@ use std::iter::FromIterator;
 ///  use vtext::metrics::string::dice_similarity;
 ///
 ///  let res = dice_similarity("yesterday", "today");
-///  assert_eq!(res, 1.0);
+///  // returns 0.333
 ///  ```
 pub fn dice_similarity(x: &str, y: &str) -> f64 {
     let mut x_tokens = Vec::new();
@@ -35,4 +35,23 @@ pub fn dice_similarity(x: &str, y: &str) -> f64 {
     let intersection_len = x_set.intersection(&y_set).count();
 
     (2 * intersection_len) as f64 / (x_set.len() + y_set.len()) as f64
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::metrics::string::dice_similarity;
+
+    fn test_dice_similarity() {
+        let res = dice_similarity("yesterday", "today");
+        assert_eq!((res * 100.).round() / 100., 0.33);
+
+        assert_eq!(dice_similarity("healed", "sealed"), 0.8);
+
+        assert_eq!(dice_similarity("", ""), 0.0);
+        // Can't make even 1 2-char ngram.
+        assert_eq!(dice_similarity("1", "test"), 0.0);
+
+        assert_eq!(dice_similarity("test", "test"), 1.0);
+    }
+
 }
