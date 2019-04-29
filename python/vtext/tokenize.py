3fd713145ed6552de26a757f55c6d6128786004e
Roman Yurchak <rth.yurchak@pm.me>
2019-04-29 18:32:49 +0200

    Relicense the python project

--- python/vtext/tokenize.py
+++ python/vtext/tokenize.py
@@ -1,3 +1,9 @@
+# Copyright 2019 vtext developers
+#
+# Licensed under the Apache License, Version 2.0,
+# <http://apache.org/licenses/LICENSE-2.0>. This file may not be copied,
+# modified, or distributed except according to those terms.
+
 from ._lib import UnicodeSegmentTokenizer
 from ._lib import RegexpTokenizer
 from ._lib import VTextTokenizer
