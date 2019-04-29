191c97afae8e6cf676d471c7c680b1762d05f7db
Roman Yurchak <rth.yurchak@pm.me>
2019-04-29 08:07:35 +0200

    Initial implementation of the Jaro similarity

--- src/metrics/string.rs
+++ src/metrics/string.rs
@@ -4,6 +4,7 @@ String metrics
 */
 use hashbrown::HashSet;
 use itertools::Itertools;
+use std::cmp::{max, min};
 use std::iter::FromIterator;
 
 ///  Sørensen–Dice similarity coefficient
@@ -46,9 +47,52 @@ pub fn dice_similarity(x: &str, y: &str) -> f64 {
     }
 }
 
+///  Jaro similarity
+pub fn jaro_similarity(x: &str, y: &str) -> f64 {
+    // implementation adapted from NLTK
+    let x_len = x.chars().count();
+    let y_len = y.chars().count();
+
+    // The upper bound of the distance for being a matched character.
+    let match_bound = max(x_len, y_len);
+    // no.of matched characters in s1 and s2
+    let mut matches = 0;
+    // no. of transpositions between s1 and s2
+    let mut transpositions = 0;
+    // positions in s1 which are matches to some character in s2
+    let mut flagged_1: Vec<usize> = Vec::new();
+    // positions in s2 which are matches to some character in s1
+    let mut flagged_2: Vec<usize> = Vec::new();
+    for (x_idx, x_char) in x.chars().enumerate() {
+        let upperbound = min(x_idx + match_bound, y_len - 1);
+        let lowerbound = max(0, x_idx as i32 - match_bound as i32) as usize;
+        for j in (lowerbound..upperbound + 1) {
+            if (x_char == y.chars().nth(j).unwrap()) & !flagged_2.contains(&j) {
+                matches += 1;
+                flagged_1.push(x_idx);
+                flagged_2.push(j);
+                break;
+            }
+        }
+        flagged_2.sort_unstable();
+    }
+    if matches == 0 {
+        return 0.0;
+    }
+    for (i, j) in flagged_1.iter().zip(flagged_2.iter()) {
+        if x.chars().nth(*i).unwrap() != y.chars().nth(*j).unwrap() {
+            transpositions += 1
+        }
+    }
+    (matches as f64 / x_len as f64
+        + matches as f64 / y_len as f64
+        + (matches as f64 - (transpositions / 2) as f64) / matches as f64)
+        / 3 as f64
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::metrics::string::dice_similarity;
+    use crate::metrics::string::*;
 
     #[test]
     fn test_dice_similarity() {
@@ -62,6 +106,12 @@ mod tests {
         assert_eq!(dice_similarity("1", "test"), 0.0);
 
         assert_eq!(dice_similarity("test", "test"), 1.0);
+    }
+
+    #[test]
+    fn test_jaro_similarity() {
+        let res = jaro_similarity("yesterday", "today");
+        assert_eq!((res * 100.).round() / 100., 0.437);
     }
 
 }
